7a59ea0842786b229891415054e26aa53f25f31a
Manank <patel.manank144@gmail.com>
2020-03-03 19:02:16 +0530

    Revert "Revert "Made workout database, final commit 03/03/2020 18:23""
    This reverts commit 37185c524f6b165831e80c7bcdd3526b1e0154b3.

--- app/src/main/java/com/manank/gymapp/DatabaseHelper.java
+++ app/src/main/java/com/manank/gymapp/DatabaseHelper.java
@@ -19,6 +19,11 @@ public class DatabaseHelper extends  SQLiteOpenHelper {
     public static final String S_COL_SESSION = "session";
     public static final String S_COL_EMAIL = "email";
 
+    public static final String TABLE_WORKOUT = "workout_table";
+    public static final String S_COL_TITLE = "title";
+    public static final String S_COL_DESCRIPTION = "description";
+    public static final String S_COL_IMAGES = "images";
+
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
         SQLiteDatabase db = this.getWritableDatabase();
@@ -26,12 +31,14 @@ public class DatabaseHelper extends  SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE "+ TABLE_SESSION +" ("+ S_COL_SESSION +" TEXT , "+ S_COL_EMAIL +" TEXT)");
+        db.execSQL("CREATE TABLE "+ TABLE_SESSION +" ("+S_COL_SESSION +" TEXT , "+ S_COL_EMAIL+ " TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_WORKOUT +" ("+ S_COL_TITLE  +" TEXT , "+ S_COL_DESCRIPTION + " TEXT , " + S_COL_IMAGES+" BLOB)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_SESSION);
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_WORKOUT);
         onCreate(db);
     }
 
@@ -75,5 +82,18 @@ public class DatabaseHelper extends  SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         db.execSQL("DROP TABLE " + TABLE_SESSION);
     }
-
+    public void setWorkout(String title,String desc,byte[] img)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(S_COL_TITLE,title);
+        cv.put(S_COL_DESCRIPTION,desc);
+        cv.put(S_COL_IMAGES,img);
+    }
+    public Cursor getWorkout()
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor data = db.rawQuery("SELECT * FROM "+TABLE_WORKOUT,null);
+        return data;
+    }
 }
