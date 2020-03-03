37185c524f6b165831e80c7bcdd3526b1e0154b3
Manank <patel.manank144@gmail.com>
2020-03-03 18:57:41 +0530

    Revert "Made workout database, final commit 03/03/2020 18:23"
    This reverts commit 86f1c323f6088bf1e486cbb5718f64c121837c5a.

--- app/src/main/java/com/manank/gymapp/ui/workout/WorkoutFragment.java
+++ app/src/main/java/com/manank/gymapp/ui/workout/WorkoutFragment.java
@@ -1,36 +1,21 @@
 package com.manank.gymapp.ui.workout;
 
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 
-import com.manank.gymapp.DatabaseHelper;
 import com.manank.gymapp.R;
 
 public class WorkoutFragment extends Fragment{
-    String[] fromColumns = {"Excercise Name", "Description","Photo"};
-    int[] toViews = {R.id.ename,R.id.descr,R.id.pic};
-    DatabaseHelper db;
-    Cursor data;
-    SimpleCursorAdapter adapter;
-    ListView workouts;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_workout, container, false);
-
-
-        db = new DatabaseHelper(getActivity().getApplicationContext());
-
-
-
         return root;
     }
 
