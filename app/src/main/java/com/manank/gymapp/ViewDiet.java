e490926135d82c2b94bbdbd21ea5d9bb097bd45f
Manank <patel.manank144@gmail.com>
2020-03-08 11:20:28 +0530

    ListView OnItemClick Viewing Added On Diet and did some minor UI update in Diet

---
+++ app/src/main/java/com/manank/gymapp/ViewDiet.java
@@ -0,0 +1,46 @@
+package com.manank.gymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.Cursor;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+
+public class ViewDiet extends AppCompatActivity {
+    DatabaseHelper db;
+    TextView title,desc,time,cal;
+    ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_diet);
+        db = new DatabaseHelper(this);
+        int id;
+        title = findViewById(R.id.vdt);
+        desc = findViewById(R.id.vdd);
+        imageView = findViewById(R.id.vdimg);
+        time = findViewById(R.id.vdtime);
+        cal = findViewById(R.id.vdcal);
+
+        Cursor data;
+        data=db.getDiet();
+        id = (int)getIntent().getLongExtra("id",0);
+
+        data.move(id);
+        title.setText(data.getString(1));
+        desc.setText(data.getString(2));
+        time.setText(time.getText().toString()+data.getString(4));
+        cal.setText(cal.getText().toString()+data.getString(5)+"kcal");
+
+        String img = data.getString(3);
+
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES+ "/FillYourFitness/Diets");
+        File imageFile = new File(path, String.valueOf(img));
+        imageView.setImageDrawable(Drawable.createFromPath(imageFile.toString()));
+    }
+}
--- app/src/main/java/com/manank/gymapp/ui/diet/DietFragment.java
+++ app/src/main/java/com/manank/gymapp/ui/diet/DietFragment.java
@@ -1,10 +1,12 @@
 package com.manank.gymapp.ui.diet;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import com.manank.gymapp.CustomDietAdapter;
 import com.manank.gymapp.DatabaseHelper;
 import com.manank.gymapp.R;
+import com.manank.gymapp.ViewDiet;
 
 public class DietFragment extends Fragment {
 
@@ -33,6 +36,15 @@ public class DietFragment extends Fragment {
             //adapter.setRoot(root);
             list.setAdapter(adapter);
         }
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent in = new Intent(getActivity().getApplicationContext(), ViewDiet.class);
+                in.putExtra("id",l);
+                startActivity(in);
+            }
+        });
 
         return root;
     }
